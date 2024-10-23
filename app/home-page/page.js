169944e1922ca77e45be7000b1e604b0d6450146
Yamini Ratnam <yamyratnam@gmail.com>
2024-10-23 12:13:49 +0530

    mod5

--- app/home-page/page.js
+++ app/home-page/page.js
@@ -277,6 +277,35 @@ export default function Page() {
             </div>
           </div>
         )}
+
+        {/* Download App Section */}
+      <section className="bg-white w-full flex flex-col items-center justify-center mt-4">
+        <div className="flex flex-col md:flex-row justify-center items-center w-full max-w-5xl">
+          {/* Play Store Link and Text */}
+          <div className="w-full md:w-1/2 p-4 flex flex-col items-left">
+            <h2 className="text-6xl font-semibold text-left text-black mb-20">
+              Download Our App on Play Store!
+            </h2>
+            <a href="https://play.google.com/store/apps/details?id=com.ywcglobal.affarmingcommunity" target="_blank" rel="noopener noreferrer" className="text-left">
+              <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M14.222 9.374c1.037-.61 1.037-2.137 0-2.748L11.528 5.04 8.32 8l3.207 2.96zm-3.595 2.116L7.583 8.68 1.03 14.73c.201 1.029 1.36 1.61 2.303 1.055zM1 13.396V2.603L6.846 8zM1.03 1.27l6.553 6.05 3.044-2.81L3.333.215C2.39-.341 1.231.24 1.03 1.27"/>
+              </svg>
+            </a>
+            <h3 className="text-2xl font-bold mt-4">Get it on Google Play</h3>
+          </div>
+
+          {/* Mobile Screenshot */}
+          <div className="w-full md:w-1/2 p-4 flex justify-center">
+            <Image
+              src="/assets/mobile.png" // Replace with your actual mobile screenshot path
+              alt="Mobile App Screenshot"
+              width={300} // Adjust width as needed
+              height={600} // Adjust height as needed
+              className="rounded-lg shadow-md"
+            />
+          </div>
+        </div>
+      </section>
   
         {/* Footer */}
         <Footer />
